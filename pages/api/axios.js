a1b8b7356f80ec5739225a7cc19002b757bb551f
Bagna Farouk Omar <91009147+omarbagna@users.noreply.github.com>
2023-08-28 17:46:03 +0000

    changed backend api

--- pages/api/axios.js
+++ pages/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://ghvip.v1-backend-api.rxhealthbeta.com/api/v1';
+const BASE_URL = 'https://gvhip.v1-backend-api.rxhealthbeta.com/api/v1';
 
 export default axios.create({
 	baseURL: BASE_URL,
